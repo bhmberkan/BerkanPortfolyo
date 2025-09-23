c6bed0f215358f6e2bab7ed89d665c2a340f01fe
bhmberkan <berkanburakturgut@gmail.com>
2025-09-24 00:22:40 +0300

    Admin panele başladım. hoşgeldiniz,  Galery ve portfolyo  için ilgli sayfaları ve kodlamaları yaptım. pagging eklicem.
    Projeler,hakkımda ve iletişim alanını kodlamadım.

---
+++ BerkanPortfolyo/Controllers/AdminPortfolyoController.cs
@@ -0,0 +1,76 @@
+﻿using BerkanPortfolyo.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BerkanPortfolyo.Controllers
+{
+    public class AdminPortfolyoController : Controller
+    {
+        BBTPortfolyoEntities db = new BBTPortfolyoEntities();
+        // GET: AdminPortfolyo
+        public ActionResult Index()
+        {
+            var values = db.Portfolio.ToList();
+            return View(values);
+        }
+
+
+        
+        [HttpGet]
+        public ActionResult CreateAdminPortfolyo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAdminPortfolyo(Portfolio t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAdminPortfolyo");
+            }
+            db.Portfolio.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult UpdatePortfolyo(int id)
+        {
+            var values = db.Portfolio.Find(id);
+            return View("UpdatePortfolyo", values);
+        }
+        [HttpPost]
+        public ActionResult UpdatePortfolyo(Portfolio t)
+        {
+            var value = db.Portfolio.Find(t.Id);
+            value.Title= t.Title;
+            value.SubTitle = t.SubTitle;
+            value.Description= t.Description;
+            value.PhotoUrl = t.PhotoUrl;
+           
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        public ActionResult DeletePortfolyo(int id)
+        {
+            var prt = db.Portfolio.Find(id);
+            db.Portfolio.Remove(prt);
+            db.SaveChanges();
+            return RedirectToAction("Index", "AdminPortfolyo");
+        }
+        
+
+
+    }
+}
