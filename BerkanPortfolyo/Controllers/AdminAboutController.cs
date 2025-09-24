5b2ad5b9cdf7277812ed89596e9f3398b48b3406
bhmberkan <berkanburakturgut@gmail.com>
2025-09-24 23:33:25 +0300

    Admin panelde eksiklikleri kodladım, Mesajlar kısmı kaldı.

---
+++ BerkanPortfolyo/Controllers/AdminAboutController.cs
@@ -0,0 +1,50 @@
+﻿using BerkanPortfolyo.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BerkanPortfolyo.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        BBTPortfolyoEntities db = new BBTPortfolyoEntities();
+        // GET: AdminAbout
+        public ActionResult Index()
+        {
+            var values  = db.About.FirstOrDefault();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult UpdateAbout(int id)
+        {
+            var values = db.About.Find(id);
+            return View("UpdateAbout", values);
+        }
+        [HttpPost]
+        public ActionResult UpdateAbout(About t)
+        {
+            var value = db.About.Find(t.ID);
+
+            value.PhotoUrl = t.PhotoUrl;
+            value.Name = t.Name;
+            value.Job  = t.Job;
+            value.SocialiconUrl = t.SocialiconUrl;
+            value.SocialiconUrl2 = t.SocialiconUrl2;
+            value.SocialiconUrl3 = t.SocialiconUrl3;
+            value.Sociallink1 = t.Sociallink1;
+            value.Sociallink2 = t.Sociallink2;
+            value.Sociallink3 = t.Sociallink3;
+            value.WhoisMe = t.WhoisMe;
+            value.WhoisMeDescription = t.WhoisMeDescription;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+       
+    }
+}
